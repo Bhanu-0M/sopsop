758ae275165db4dba9185fe9993a0c7a9a34db51
Bhanu-0M <118541518+Bhanu-0M@users.noreply.github.com>
2023-08-02 15:45:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function f()
+    {
+    let a =document.getElementById("input1").value;
+    let b =document.getElementById("input2").value;
+    let c =document.getElementById("input3").value;
+    let d =document.getElementById("input4").value;
+    let e =document.getElementById("input5").value;
+
+    
+    sessionStorage.setItem("p1",a);
+    sessionStorage.setItem("p2",b);
+    sessionStorage.setItem("p3",c);
+    sessionStorage.setItem("p4",d);
+    sessionStorage.setItem("p5",e);
+
+}
---
+++ script1.js
@@ -0,0 +1,11 @@
+var p1=sessionStorage.getItem('p1');
+var p2=sessionStorage.getItem('p2');
+var p3=sessionStorage.getItem('p3');
+var p4=sessionStorage.getItem('p4');
+var p5=sessionStorage.getItem('p5');
+
+document.getElementById("p1").textContent=p1;
+document.getElementById("p2").textContent=p2;
+document.getElementById("p3").textContent=p3;
+document.getElementById("p4").textContent=p4;
+document.getElementById("p5").textContent=p5;
